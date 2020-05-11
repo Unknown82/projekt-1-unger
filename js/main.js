8cc7bcb449080c5ddfa8f2c540b9d3394d2bc8b6
Andreas Unger <andreas82un@web.de>
2020-05-11 19:45:45 +0200

    js

--- js/main.js
+++ js/main.js
@@ -4,20 +4,6 @@ window.addEventListener('DOMContentLoaded', function() {
 
         // Slider
 
-    const entities = [
-        {
-            stadt: 'Rostov-on-Don<br> LCD admiral',
-            slides: 'https://www.purinaone.ru/cat/sites/purinaone.ru/files/2019-03/kittens-cat3.jpg'
-        },
-        {
-            stadt: 'Sochi<br> Thieves',
-            slides: 'https://s1.stc.all.kpcdn.net/putevoditel/projectid_103889/images/tild3463-3139-4630-b563-646166616434__20180225_gaf_uw8_101.jpg'
-        },
-        {
-            stadt: 'Rostov-on-Don<br> Patriotic,
-            slides: 'https://whiskas.ru/upload/medialibrary/392/%D0%9A%D0%BE%D0%B3%D0%B4%D0%B0_%D1%83_%D0%BA%D0%BE%D1%82%D0%B5%D0%BD%D0%BA%D0%B0_%D0%BE%D1%82%D1%80%D1%8B%D0%B2%D0%B0%D1%8E%D1%82%D1%81%D1%8F_%D0%B3%D0%BB%D0%B0%D0%B7%D0%BA%D0%B8_1404x879.png'
-        }
-        ]
     let slideIndex = 1,
         slides = document.querySelectorAll('.control-slayer__img'),
         prev = document.querySelector('.control-pfeil__left'),
